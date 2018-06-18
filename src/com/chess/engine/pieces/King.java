88fafe27b992e64304272469dcfb07e29b26b6b2
RagingParth <pm474@njit.edu>
2018-06-18 01:46:16 -0400

    Add King piece

---
+++ src/com/chess/engine/pieces/King.java
@@ -0,0 +1,19 @@
+package com.chess.engine.pieces;
+
+import com.chess.engine.Alliance;
+import com.chess.engine.board.Board;
+import com.chess.engine.board.Move;
+
+import java.util.Collection;
+
+public class King extends Piece {
+
+    King(int piecePosition, Alliance pieceAlliance) {
+        super(piecePosition, pieceAlliance);
+    }
+
+    @Override
+    public Collection<Move> calculateLegalMoves(Board board) {
+        return null;
+    }
+}
